418ac0bfedb39553745868e52333c1fddf950fd0
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-02-06 13:51:47 +0530

    Create LINE FOLLOWING BOT.C

---
+++ LINE FOLLOWING BOT.C
@@ -0,0 +1,53 @@
+// Motor A connections
+int enA = 5;
+int in1 = 11;
+int in2 = 10;
+// Motor B connections
+int enB = 6;
+int in3 = 9;
+int in4 = 8;
+#define R_S A2//ir sensor Right
+#define L_S A3 //ir sensor Left
+void setup(){ 
+pinMode(R_S, INPUT); 
+pinMode(L_S, INPUT); 
+pinMode(enA, OUTPUT); 
+pinMode(in1, OUTPUT); 
+pinMode(in2, OUTPUT); 
+pinMode(in3, OUTPUT); 
+pinMode(in4, OUTPUT); 
+pinMode(enB, OUTPUT);
+digitalWrite(enA, HIGH); 
+digitalWrite(enB, HIGH); 
+delay(1000);
+}
+void loop(){  
+if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 0)){forward();}   //if Right Sensor and Left Sensor are at White color then it will call forword function
+if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 0)){turnRight();} //if Right Sensor is Black and Left Sensor is White then it will call turn Right function  
+if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 1)){turnLeft();}  //if Right Sensor is White and Left Sensor is Black then it will call turn Left function
+if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 1)){Stop();} //if Right Sensor and Left Sensor are at Black color then it will call Stop function
+}
+void forward(){  //forword
+digitalWrite(in1, HIGH); //Right Motor forword Pin 
+digitalWrite(in2, LOW);  //Right Motor backword Pin 
+digitalWrite(in3, LOW);  //Left Motor backword Pin 
+digitalWrite(in4, HIGH); //Left Motor forword Pin 
+}
+void turnRight(){ //turnRight
+digitalWrite(in1, LOW);  //Right Motor forword Pin 
+digitalWrite(in2, HIGH); //Right Motor backword Pin  
+digitalWrite(in3, LOW);  //Left Motor backword Pin 
+digitalWrite(in4, HIGH); //Left Motor forword Pin 
+}
+void turnLeft(){ //turnLeft
+digitalWrite(in1, HIGH); //Right Motor forword Pin 
+digitalWrite(in2, LOW);  //Right Motor backword Pin 
+digitalWrite(in3, HIGH); //Left Motor backword Pin 
+digitalWrite(in4, LOW);  //Left Motor forword Pin 
+}
+void Stop(){ //stop
+digitalWrite(in1, LOW); //Right Motor forword Pin 
+digitalWrite(in2, LOW); //Right Motor backword Pin 
+digitalWrite(in3, LOW); //Left Motor backword Pin 
+digitalWrite(in4, LOW); //Left Motor forword Pin 
+}
